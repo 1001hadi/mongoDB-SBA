79e4643812909deb6050a1d3e0c0a2a0892f4ae2
hadi.jaf <hadi.jaf.1001@gmail.com>
2025-04-29 21:08:22 -0600

    task routes logic file added and all task routes logic moved to

--- routes/taskRoutes.mjs
+++ routes/taskRoutes.mjs
@@ -1,86 +1,35 @@
 import express from "express";
-import Tasks from "../models/taskSchema.mjs";
-import seedTasks from "../utilities/seedData.mjs";
+import {
+  createTask,
+  getAllTasks,
+  getTask,
+  updateTask,
+  deleteTask,
+  searchTask,
+  addSeedTask,
+} from "../routesLogic/taskRoutesLogic.mjs";
 
 const router = express.Router();
 
 // search route
-router.get("/search", async (req, res) => {
-  // console.log(searchTerm);
-  const searchQuery = req.query.query;
-
-  if (!searchQuery) {
-    return res.status(400).json({ msg: "Missing search query" });
-  }
-
-  try {
-    const tasks = await Tasks.find({
-      title: { $regex: searchQuery, $options: "i" },
-    });
-
-    res.status(200).json(tasks);
-  } catch (err) {
-    res.status(500).json({ msg: err.message });
-  }
-});
+router.get("/search", searchTask);
 
 // create
-router.post("/", async (req, res) => {
-  let newTask = await new Tasks(req.body);
-  await newTask.save();
-  res.json(newTask).status(201);
-});
+router.post("/", createTask);
 
 // readAll
-router.get("/", async (req, res) => {
-  let getTasks = await Tasks.find({});
-  res.json(getTasks).status(200);
-});
+router.get("/", getAllTasks);
 
 // read by id
-router.get("/:id", async (req, res) => {
-  let getTask = await Tasks.findById(req.params.id);
-
-  if (!getTask) {
-    return res.json({ msg: `task not fund!` }).status(404);
-  }
-
-  res.json(getTask).status(200);
-});
+router.get("/:id", getTask);
 
 // update
-router.put("/:id", async (req, res) => {
-  let updateTask = await Tasks.findByIdAndUpdate(req.params.id, req.body, {
-    new: true,
-  });
-
-  if (!updateTask) {
-    res.json({ msg: "can't update the given task!" }).status(400);
-  }
-
-  res.json(updateTask).status(201);
-});
+router.put("/:id", updateTask);
 
 // delete
-router.delete("/:id", async (req, res) => {
-  let deleteTask = await Tasks.findByIdAndDelete(req.params.id);
-
-  res.json(deleteTask).status(200);
-});
+router.delete("/:id", deleteTask);
 
 // // seed route
-router.post("/seed", async (req, res) => {
-  try {
-    await Tasks.deleteMany({});
-    const addSeedTask = await Tasks.insertMany(seedTasks);
-
-    res.status(201).json({
-      message: "Seed task added!",
-      data: addSeedTask,
-    });
-  } catch (err) {
-    console.error("Error adding seed tasks:", err);
-  }
-});
+router.post("/seed", addSeedTask);
 
 export default router;
---
+++ routesLogic/taskRoutesLogic.mjs
@@ -0,0 +1,80 @@
+import Tasks from "../models/taskSchema.mjs";
+import seedTasks from "../utilities/seedData.mjs";
+
+// create task
+export async function createTask(req, res) {
+  let newTask = await new Tasks(req.body);
+  await newTask.save();
+  res.json(newTask).status(201);
+}
+
+// get all tasks
+export async function getAllTasks(req, res) {
+  let getTasks = await Tasks.find({});
+  res.json(getTasks).status(200);
+}
+
+// get specific task
+export async function getTask(req, res) {
+  let getTask = await Tasks.findById(req.params.id);
+
+  if (!getTask) {
+    return res.json({ msg: `task not fund!` }).status(404);
+  }
+
+  res.json(getTask).status(200);
+}
+
+// update task
+export async function updateTask(req, res) {
+  let updateTask = await Tasks.findByIdAndUpdate(req.params.id, req.body, {
+    new: true,
+  });
+
+  if (!updateTask) {
+    res.json({ msg: "can't update the given task!" }).status(400);
+  }
+
+  res.json(updateTask).status(201);
+}
+
+// delete task
+export async function deleteTask(req, res) {
+  let deleteTask = await Tasks.findByIdAndDelete(req.params.id);
+
+  res.json(deleteTask).status(200);
+}
+
+// search task
+export async function searchTask(req, res) {
+  const searchQuery = req.query.query;
+
+  if (!searchQuery) {
+    return res.status(400).json({ msg: "Missing search query" });
+  }
+
+  try {
+    const tasks = await Tasks.find({
+      title: { $regex: searchQuery, $options: "i" },
+    });
+
+    res.status(200).json(tasks);
+  } catch (err) {
+    res.status(500).json({ msg: err.message });
+  }
+}
+
+// add seed task data
+export async function addSeedTask(req, res) {
+  try {
+    await Tasks.deleteMany({});
+    const addSeedTask = await Tasks.insertMany(seedTasks);
+
+    res.status(201).json({
+      message: "Seed task added!",
+      data: addSeedTask,
+    });
+  } catch (err) {
+    console.error("Error adding seed tasks:", err);
+  }
+}
